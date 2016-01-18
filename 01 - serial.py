26287c00a6fcc233365e19d2daddd905184dad96
Daniel Vonk <dvonk1991@gmail.com>
2016-01-18 06:25:25 +0100

    Serial computation in Python 2.7

---
+++ 01 - serial.py
@@ -0,0 +1,19 @@
+from timeit import default_timer as timer
+
+def estimate_pi(n):   
+    count = 0 
+    for i in range(n):
+        x = ((i - 0.5) / n)
+        count += 4.0 / (1 + x * x)       
+    return count
+
+n = 100000000
+
+start = timer()
+pi = estimate_pi(n)/n
+end = timer()
+
+print (pi)
+print "Time:", end - start
+ 
+
