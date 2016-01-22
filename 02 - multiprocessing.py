b65558e66b3bfdca9c3a4988e46604dbad9d4998
Daniel Vonk <dvonk1991@gmail.com>
2016-01-22 13:00:58 +0100

    Parallel computation using multiprocessing

--- 02 - multiprocessing.py
+++ 02 - multiprocessing.py
@@ -2,18 +2,22 @@ import multiprocessing
 from multiprocessing import Pool
 from timeit import default_timer as timer
 
+chunk_size = 1000000
+N = 100000000
+
 #========== Estimate Pi (according to lecture slides) ==========
 def estimate_pi(n):   
     count = 0 
-    for i in range(n):
-        x = ((i - 0.5) / n)
-        count += 4.0 / (1 + x * x)       
+    for i in range(chunk_size*(n-1), chunk_size*n):
+        x = ((i - 0.5) / N)
+        count += 4.0 / (1 + x**2)       
     return count
 
 #========== Parallel computing via multiprocessing map ==========
 def calculate_pi(cpus, n):
     #Divide workload (n) on number of CPUs
-    part_count = [n/cpus for i in range(cpus)]
+    
+    part_count = [i for i in range(1, int(round(n/chunk_size) + 1))]
     #Define pool with one process per CPU
     pool = Pool(processes = cpus)
 
@@ -23,9 +27,9 @@ def calculate_pi(cpus, n):
     end = timer() 
 
     #Print some stuff
-    print "Using", cpus, "CPU(s)"
-    print "Time:", end - start
-    print "--------------------------"
+    print("Using " + str(cpus) + " CPU(s)") 
+    print("Time: " + str(end - start))
+    print("--------------------------")
 
     #Divide by n (according to lecture slides)
     pi = sum(count)/n
@@ -35,17 +39,16 @@ def calculate_pi(cpus, n):
 if __name__=='__main__':
 
     #Define n and number of CPUs
-    n = 100000000
     pi = 0
     cpus = multiprocessing.cpu_count()
-    print "CPU count:", cpus
-    print "n =", n
-    print "--------------------------"
-    print "--------------------------"
+    print("CPU count: " + str(cpus))
+    print("n = " +  str(N))
+    print("--------------------------")
+    print("--------------------------")
 
     #Estimate Pi
     for i in range (cpus):
-        pi += calculate_pi(i+1, n)
+        pi += calculate_pi(i+1, N)
 
-    print "--------------------------"
-    print "Pi: ", pi/cpus
+    print("--------------------------")
+    print("Pi: " +  str(pi/cpus))
